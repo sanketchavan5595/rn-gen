61c416d1a172604798ae3da2d573f7a51f374a1e
sanketchavan5595 <sanketchavan5595@gmail.com>
2022-08-06 10:39:20 +0530

    user input and file creation done

---
+++ constants.js
@@ -0,0 +1,26 @@
+export const FILE_TYPES = {
+  Utility: "Utility",
+  Constant: "Constant",
+  CSS: "CSS",
+  Interface: "Interface",
+  Storybook: "Storybook",
+};
+
+export const FILE_TYPE_CHOICES = [
+  "Utility",
+  "Constant",
+  "CSS",
+  "Interface",
+  "Storybook",
+];
+
+export const EXTENSION_CHOICES = ["jsx", "tsx"];
+
+export const DEFAULT_COMPONENT_NAME = "Component";
+export const DEFAULT_EXTENSION = "jsx";
+
+export const USER_INPUT_TYPES = {
+  INPUT: "input",
+  LIST: "list",
+  CHECKBOX: "checkbox",
+};
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
-import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 import inquirer from "inquirer";
+import fs from "node:fs";
+import {
+  DEFAULT_COMPONENT_NAME,
+  DEFAULT_EXTENSION,
+  EXTENSION_CHOICES,
+  FILE_TYPE_CHOICES,
+  USER_INPUT_TYPES,
+} from "./constants.js";
+import {
+  getComponentFileName,
+  getOtherFileExtensions,
+  getOtherFilesNames,
+  sleep,
+} from "./utils.js";
 
-let componentName = "Component";
-
-// sleep function
-const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+let componentName = DEFAULT_COMPONENT_NAME;
+let extension = DEFAULT_EXTENSION;
+let otherFileNames = [];
 
 const showTitleText = async () => {
   const titleText = chalkAnimation.pulse("Generate a component");
@@ -14,17 +26,53 @@ const showTitleText = async () => {
   titleText.stop();
 };
 
-await showTitleText();
-
 const getComponentName = async () => {
   const userInput = await inquirer.prompt({
     name: "name",
-    type: "input",
+    type: USER_INPUT_TYPES.INPUT,
     default: () => "Component",
+    message: "Enter component name",
   });
   componentName = userInput.name;
 };
 
-await getComponentName();
+const getExtension = async () => {
+  const userInput = await inquirer.prompt({
+    name: "extension",
+    type: USER_INPUT_TYPES.LIST,
+    choices: EXTENSION_CHOICES,
+    message: "Select an extension (default: jsx)",
+  });
 
-console.log(componentName);
+  extension = userInput.extension;
+};
+
+const getOtherChoices = async () => {
+  const userInput = await inquirer.prompt({
+    name: "other",
+    type: USER_INPUT_TYPES.CHECKBOX,
+    message: "Other files: ",
+    choices: FILE_TYPE_CHOICES,
+  });
+  otherFileNames = userInput.other.map((item) =>
+    getOtherFilesNames(item, extension, componentName)
+  );
+};
+
+await showTitleText();
+await getComponentName();
+await getExtension();
+await getOtherChoices();
+
+const allFiles = [
+  getComponentFileName(componentName, extension),
+  ...otherFileNames,
+  `index.${getOtherFileExtensions(extension)}`,
+];
+
+fs.mkdirSync(`${componentName}`);
+
+allFiles.forEach((item) => {
+  const path = `${componentName}/${item}`;
+  fs.writeFileSync(path, "");
+});
